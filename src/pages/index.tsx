ee8792da359e1cc197f4b54d4cc67a8523a0e44d
cloudIris75 <gmyeom1230@gmail.com>
2022-10-26 16:20:36 +0900

    Design: index width 조절

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,27 +13,28 @@ const Home: NextPage = () => {
   return (
     <>
       <Navbar />
-      <main className="py-8 px-12 sm:px-48">
+      <main className="py-8">
         <div className="flex flex-col items-center space-y-10">
-          <div className="w-60">
-            <Image
-              alt="logo"
-              src="https://gifty-bucket.s3.ap-northeast-2.amazonaws.com/images/logo.png"
-              layout="responsive"
-              width={100}
-              height={50}
-              className="rounded-3xl"
-            />
-          </div>
           <h1 className="text-2xl text-coffee-400 font-bold">
-            기프티콘으로 결제할 때, <br className="md:hidden" />더 이상
+            기프티콘으로 결제할 때, <br className="lg:hidden" />더 이상
             머뭇거리지 마세요!
           </h1>
-          <p className="bg-coffee-100 text-coffee-400 rounded-3xl px-6 py-8">
-            계산대 앞에서 우물쭈물하느라 뒷사람 눈치 보는 건 이제 그만! 원하는
-            메뉴 구성으로 쉽고 빠르게 계산하여 기프티콘을 사용할 수 있습니다.
-            (문구 수정 예정)
-          </p>
+          <div className="flex flex-col items-center space-y-10 bg-white rounded-3xl px-6 py-8 w-64 sm:w-1/2 lg:w-1/3">
+            <div className="w-60">
+              <Image
+                alt="logo"
+                src="https://gifty-bucket.s3.ap-northeast-2.amazonaws.com/images/logo.png"
+                layout="responsive"
+                width={100}
+                height={50}
+              />
+            </div>
+            <p className="text-coffee-400">
+              계산대 앞에서 우물쭈물하느라 뒷사람 눈치 보는 건 이제 그만! 원하는
+              메뉴 구성으로 쉽고 빠르게 계산하여 기프티콘을 사용할 수 있습니다.
+              (문구 수정 예정)
+            </p>
+          </div>
           <button
             onClick={onMoveButtonClick}
             className="bg-coffee-400 px-6 py-2 rounded-3xl"

5e434cbf364b86da2f4f0c47af231b6516365309
cloudIris75 <gmyeom1230@gmail.com>
2022-10-26 15:02:47 +0900

    Feat: brands 페이지 추가

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
         <a className="font-bold text-xl">GIFTY</a>
       </Link>
       <Link href="https://litt.ly/iris">
-        <a>문의</a>
+        <a className="bg-white px-4 py-2 text-coffee-400 rounded-3xl">문의</a>
       </Link>
     </div>
   );
--- src/pages/brands/index.tsx
+++ src/pages/brands/index.tsx
@@ -1,9 +1,31 @@
+import Link from 'next/link';
 import Navbar from '../../components/Navbar';
 
 const Brands: React.FC = () => {
   return (
     <>
       <Navbar />
+      <main className="flex justify-center py-8">
+        <div className="flex flex-col space-y-8">
+          <Link href="/brands/starbucks/calculator">
+            <a className="brand-container">스타벅스</a>
+          </Link>
+          <Link href="/brands/twosome/calculator">
+            <a className="brand-container bg-coffee-300 pointer-events-none">
+              투썸플레이스
+              <br />
+              (예정)
+            </a>
+          </Link>
+          <Link href="/brands/ediya/calculator">
+            <a className="brand-container bg-coffee-300 pointer-events-none">
+              이디야커피
+              <br />
+              (예정)
+            </a>
+          </Link>
+        </div>
+      </main>
     </>
   );
 };

0f6c66e5a72ff243c84182513289fc0f9a848583
cloudIris75 <gmyeom1230@gmail.com>
2022-10-28 13:42:37 +0900

    Design: 계산기 영수증 디자인으로 변경

--- src/pages/brands/[brandName]/calculator.tsx
+++ src/pages/brands/[brandName]/calculator.tsx
@@ -11,28 +11,42 @@ const Calculator: React.FC = () => {
   return (
     <>
       <Navbar />
-      <main className="flex flex-col items-center space-y-8 p-8">
-        <div>
-          <h3 className="title-container">기프티콘</h3>
-        </div>
-        <div className="flex flex-col space-y-2 items-center w-full">
-          <hr className="border w-full" />
-          <p>최종 계산 결과는 하단에서 확인할 수 있습니다.</p>
-        </div>
-        <div>
-          <h3 className="title-container">메뉴 1</h3>
-        </div>
-        <div>
-          <h3 className="title-container">메뉴 2</h3>
-        </div>
-        <div>
-          <h3 className="title-container">메뉴 3</h3>
-        </div>
-        <div className="flex flex-col w-full text-right">
-          <hr className="border w-full mb-2" />
-          <p className="font-bold text-xl">n원</p>
-          <p>기프티콘 가격보다 n원 부족해요!</p>
-          <p>n원어치 추가 선택해 주세요.</p>
+      <main className="flex flex-col items-center m-8 space-y-8">
+        <div className="flex flex-col space-y-8 px-4 py-6 bg-white w-full">
+          <h3 className="text-coffee-400 text-center font-bold text-xl">
+            GIFTY
+          </h3>
+          <div>
+            <h3 className="title-container">기프티콘</h3>
+          </div>
+          <div className="flex flex-col space-y-2 items-center w-full">
+            <hr className="border-dashed border-coffee-400 w-full" />
+            <p className="text-coffee-400 text-sm">
+              최종 계산 결과는 하단에서 확인할 수 있습니다.
+            </p>
+          </div>
+          <div>
+            <h3 className="title-container">메뉴 1</h3>
+          </div>
+          <div>
+            <h3 className="title-container">메뉴 2</h3>
+          </div>
+          <div>
+            <h3 className="title-container">메뉴 3</h3>
+          </div>
+          <div className="flex flex-col w-full text-right">
+            <hr className="border-dashed border-coffee-400 w-full mb-2" />
+            <div className="flex justify-between font-bold text-lg mb-2">
+              <p className="text-coffee-400">결제금액</p>
+              <p className="text-coffee-400">n원</p>
+            </div>
+            <p className="text-coffee-400 text-sm">
+              기프티콘 가격보다 n원 부족해요!
+            </p>
+            <p className="text-coffee-400 text-sm">
+              n원어치 추가 선택해 주세요.
+            </p>
+          </div>
         </div>
         <div className="flex flex-col space-y-4">
           <Button
